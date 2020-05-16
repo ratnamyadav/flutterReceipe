36f442b4b962bf2cee57fa64546b846ee7e65705
Ratnam Yadav <ratnam.yadav@icloud.com>
2020-05-16 20:06:32 +0530

    basic structure of app with dummy data

---
+++ android/app/src/main/kotlin/com/ratnamy/meals/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ratnamy.meals
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/category_meals_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Meals'),
+      ),
+      body: Center(
+        child: Text('The receipe of category'),
+      ),
+    );
+  }
+
+}
